1e5f190776ba51cf2168bef6a7bd5f45276fe4da
DIANELLY VENTURA <DMVCode@outlook.com>
2023-03-19 10:57:31 -0400

    Migrate routines model into database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_19_144147) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_19_145252) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_19_144147) do
     t.text "description"
     t.string "image_url"
     t.string "video_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "routines", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "excercise_id"
+    t.integer "reps"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
