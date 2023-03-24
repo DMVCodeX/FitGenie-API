e0ee4506ced1beaa070cfae322da92d531332b99
DIANELLY VENTURA <DMVCode@outlook.com>
2023-03-23 20:52:27 -0400

    remove user_id column from workout model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_24_003357) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_24_004919) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,9 +34,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_003357) do
   end
 
   create_table "workouts", force: :cascade do |t|
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
   end
+
 end
